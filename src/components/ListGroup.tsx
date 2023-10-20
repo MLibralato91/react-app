83dbad5e3206ef9238d841adb344d1b76375f8f9
MLibralato91 <michelelibralato911@gmail.com>
2023-10-20 17:20:48 +0200

    add first useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 function ListGroup() {
+  let items = ["New York", "Paris", "Rome", "Milan", "Turin"];
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
     <>
       <div className="container">
         <h1>List Group</h1>
+        {items.length === 0 && <p>No item fount</p>}
         <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
+          {items.map((item, index) => (
+            <li
+              className={
+                selectedIndex === index
+                  ? "list-group-item active"
+                  : "list-group-item"
+              }
+              key={item}
+              onClick={() => {
+                setSelectedIndex(index);
+              }}
+            >
+              {item}
+            </li>
+          ))}
         </ul>
       </div>
     </>
